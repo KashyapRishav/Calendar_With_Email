6b14525e29c2150b511241defead784321d4adee
Rishi <kashyaprishav96@gmail.com>
2021-11-25 12:20:15 +0530

    Final Commit

---
+++ app/src/main/java/com/rishav/calendar/MailUtils.java
@@ -0,0 +1,8 @@
+package com.rishav.calendar;
+
+public class MailUtils {
+    // HERE PUT EMAIL AND PASSWORD , LIKE example@gmail.com
+
+    public static final  String EMAIL = "@gmail.com";
+    public static final String PASSWORD = "password";
+}
